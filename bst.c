1b728162776705859e3a245c392f850dccaeb479
nithish1501 <nithishanish2007@gmail.com>
2026-02-15 17:47:57 +0530

    Add files via upload

---
+++ bst.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+struct Node* insert(struct Node* root, int value) {
+    if(root == NULL)
+        return createNode(value);
+
+    if(value < root->data)
+        root->left = insert(root->left, value);
+    else if(value > root->data)
+        root->right = insert(root->right, value);
+
+    return root;
+}
+
+void inorder(struct Node* root) {
+    if(root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int search(struct Node* root, int key) {
+    if(root == NULL)
+        return 0;
+    if(root->data == key)
+        return 1;
+    else if(key < root->data)
+        return search(root->left, key);
+    else
+        return search(root->right, key);
+}
+
+int main() {
+    struct Node* root = NULL;
+    int choice, value;
+
+    do {
+        printf("\n--- Binary Search Tree ---\n");
+        printf("1. Insert\n");
+        printf("2. Display (Inorder)\n");
+        printf("3. Search\n");
+        printf("4. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter value: ");
+                scanf("%d", &value);
+                root = insert(root, value);
+                break;
+
+            case 2:
+                printf("Inorder Traversal: ");
+                inorder(root);
+                printf("\n");
+                break;
+
+            case 3:
+                printf("Enter value to search: ");
+                scanf("%d", &value);
+                if(search(root, value))
+                    printf("Value found!\n");
+                else
+                    printf("Value not found!\n");
+                break;
+
+            case 4:
+                printf("Exiting...\n");
+                break;
+
+            default:
+                printf("Invalid choice!\n");
+        }
+
+    } while(choice != 4);
+
+    return 0;
+}
